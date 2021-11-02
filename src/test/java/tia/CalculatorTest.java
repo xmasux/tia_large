7ab5a598bd0d56f5822b1e1adb9415d84a3efe51
Marius Mergenthaler <mergenthaler@cqse.eu>
2021-11-02 10:14:09 +0100

    remove comments

--- src/test/java/tia/CalculatorTest.java
+++ src/test/java/tia/CalculatorTest.java
@@ -4,58 +4,48 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/** Class comment. */
 public class CalculatorTest {
 
-    /** Test comment. */
     @Test
     public void testSum() {
         assertEquals(3, new Calculator().sum(1, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testMinus() {
         assertEquals(1, new Calculator().minus(3, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testSum2() {
         assertEquals(1, new Calculator().sum(-1, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testMultiply() {
         assertEquals(6, new Calculator().multiply(3, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testDivide() {
         assertEquals(1, new Calculator().divide(3, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testSumFloat() {
         assertEquals(5.0, new Calculator().sumFloat(3, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testMinusFloat() {
         assertEquals(1, new Calculator().minusFloat(3, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testMultiplyFloat() {
         assertEquals(6.0, new Calculator().multiplyFloat(3, 2));
     }
 
-    /** Test comment. */
     @Test
     public void testDivideFloat() {
         assertEquals(1.5, new Calculator().divideFloat(3, 2));
